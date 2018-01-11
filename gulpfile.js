54942d539ff9ce9107d954ccf75b5593f2d996e4
EimD <eimdumse@gmail.com>
2018-01-11 16:18:17 +0200

    Made commented changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,64 @@
-const gulp = require('gulp'),
-    clean = require('gulp-clean');
-    browserSync = require('browser-sync').create(),
-    sass = require('gulp-sass');
-    autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp"),
+    gulpsync = require('gulp-sync')(gulp);
+    clean = require("gulp-clean");
+    browserSync = require("browser-sync").create(),
+    sass = require("gulp-sass");
+    imagemin = require('gulp-imagemin');
+    autoprefixer = require("gulp-autoprefixer");
 
-gulp.task('clean', function () {
-    return gulp.src("src/css")
+const paths = {
+    src: "./src",
+    build: "./build",
+
+    srcSass: ["./src/sass/*.scss"],
+    srcImg: ["./src/images/*"],
+    srcHtml: "./src/index.html",
+
+    buildCss: "./build/css",
+    buildHtml: "./build/*.html",
+    buildImg: "./build/images"
+
+}
+
+gulp.task("clean", () => {
+    return gulp.src(paths.build)
         .pipe(clean());
 });
 
 // Compile Sass
-gulp.task('sass', ['clean'],() => {
-    return gulp.src(["src/sass/*.scss"])
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+gulp.task("sass",() => {
+    return gulp.src(paths.srcSass)
+        .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 3 versions', '> 1%', 'IE 9', 'IE 10', 'IE 11', 'Firefox ESR'],
+            browsers: ["last 3 versions", "> 1%", "IE 9", "IE 10", "IE 11", "Firefox ESR"],
             cascade: false
         }))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest(paths.buildCss))
         .pipe(browserSync.stream());
 });
 
+// Imagemin images and ouput them in dist
+gulp.task('imagemin', () => {
+    gulp.src(paths.srcImg)
+    .pipe(gulp.dest(paths.buildImg));
+   });
+
+// Copy index.html to dist directly
+gulp.task('copyHtml', () => {
+    // Copy html
+    gulp.src(paths.srcHtml)
+    .pipe(gulp.dest(paths.build))
+});
+
 // Watch & Serve
-gulp.task('serve', ['sass'], () => {
+gulp.task("serve", ["sass", "copyHtml"], () => {
     browserSync.init({
-        server: './src'
+        server: paths.build
     });
 
-    gulp.watch(["src/sass/*.scss"], ['sass']);
-    gulp.watch(['./src/**/*.html']).on('change', browserSync.reload);
+    gulp.watch(paths.srcSass, ["sass"]);
+    gulp.watch(paths.srcHtml, ["copyHtml"]).on("change", browserSync.reload);
 });
 
 // Default
-gulp.task('default', ['serve']);
+gulp.task("default", gulpsync.sync(["clean", "imagemin", "copyHtml", "serve"]));
